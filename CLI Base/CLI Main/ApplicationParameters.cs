a344555f4ec81c83e69a3174ac92ec1858d5932c
Raul Santana <raul.santana@eletraenergy.com>
2023-02-01 12:28:49 -0300

    Adicionada classe de parâmetros da aplicação, com o método de obter a versão da aplicação.

---
+++ CLI Base/CLI Main/ApplicationParameters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI.Main
+{
+    public static class ApplicationParameters
+    {
+        private static uint _primaryVersion = 1;
+        private static uint _secondaryVersion = 0;
+        private static uint _revisionBugs = 0;
+
+        public static string GetVersion()
+        {
+            return $"{_primaryVersion}.{_secondaryVersion}.{_revisionBugs}";
+        }
+    }
+}
